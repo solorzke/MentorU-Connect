2d798ce676c46465b963e2ea85439c10e769ac79
Kevin Solorzano <solorzke@gmail.com>
2019-03-18 15:42:01 -0400

    Misc

--- app/src/main/java/com/example/mentorapp/Home.java
+++ app/src/main/java/com/example/mentorapp/Home.java
@@ -1,6 +1,5 @@
 package com.example.mentorapp;
 
-import android.content.Context;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,11 +9,8 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.JsonRequest;
 import com.android.volley.toolbox.Volley;
-import com.example.mentorapp.model.User;
 
 import org.json.JSONArray;
 import org.json.JSONException;
