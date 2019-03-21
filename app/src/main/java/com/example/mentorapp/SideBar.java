c39a8323e7899db8508ba4b71b9eb6fe148c7d19
Kevin Solorzano <solorzke@gmail.com>
2019-03-21 15:38:06 -0400

    Update name/email header from the side bar drawer with user's information

--- app/src/main/java/com/example/mentorapp/SideBar.java
+++ app/src/main/java/com/example/mentorapp/SideBar.java
@@ -1,6 +1,9 @@
 package com.example.mentorapp;
 
 import android.app.ActionBar;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -10,18 +13,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class SideBar extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private DrawerLayout drawer;
-    TextView title;
+    TextView title, user_name, user_email;
+    SharedPreferences SESSION;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sidebar);
 
+        SESSION = getSharedPreferences("USER", MODE_PRIVATE);
+
+        //POSITION THE TOOLBAR'S TITLE TO THE FAR RIGHT
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayOptions(android.support.v7.app.ActionBar.DISPLAY_SHOW_CUSTOM);
@@ -30,19 +40,32 @@ public class SideBar extends AppCompatActivity implements NavigationView.OnNavig
 
         drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
+
+        //UPDATE HEADER NAME AND EMAIL OF THE USER IN SESSION
+        View headerView = navigationView.getHeaderView(0);
+        user_name = (TextView) headerView.findViewById(R.id.header_name);
+        String header_name = SESSION.getString("fname", null) + " " +
+                SESSION.getString("lname", null);
+        user_name.setText(header_name);
+        user_email = (TextView) headerView.findViewById(R.id.header_email);
+        String header_email = SESSION.getString("email", null);
+        user_email.setText(header_email);
+
+        //SET ACTIONBAR HAMBURGER TO TOGGLE THE SIDEBAR NAVIGATION WHEN PRESSED
         navigationView.setNavigationItemSelectedListener(this);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
                 R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
+        //THIS IS THE DEFAULT FRAGMENT TO OPEN INTO WHEN YOU LOGIN IN - HOME
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
                 new HomeFragment()).commit();
         navigationView.setCheckedItem(R.id.home_item);
 
-
     }
 
+    //OPEN A FRAGMENT IF A MENU ITEM IS SELECTED FROM THE SIDE BAR MENU
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
         switch (menuItem.getItemId()){
@@ -57,8 +80,8 @@ public class SideBar extends AppCompatActivity implements NavigationView.OnNavig
                         new HomeFragment()).commit();
                 break;
             case R.id.log_out:
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
-                        new HomeFragment()).commit();
+                Intent i = new Intent(getApplicationContext(), Login.class);
+                startActivity(i);
                 break;
             case R.id.well_being:
                 title.setText("Well Being");
