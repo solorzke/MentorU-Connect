d62a70984a1c73d57c1630ef7f2d9e0ace73ba2b
Kevin Solorzano <solorzke@gmail.com>
2019-04-09 18:11:17 -0400

    Article for Well Being created to begin designing.

---
+++ app/src/main/java/com/example/mentorapp/WellBeing/WB_Article1.java
@@ -0,0 +1,36 @@
+package com.example.mentorapp.WellBeing;
+
+import android.content.Intent;
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+import com.example.mentorapp.R;
+import com.example.mentorapp.WBFragment;
+
+public class WB_Article1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wb__article1);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayOptions(android.support.v7.app.ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
