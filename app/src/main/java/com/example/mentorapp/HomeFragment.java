bc56235b5218f4f3dde0442e43de1eed4d3083e2
Kevin Solorzano <solorzke@gmail.com>
2019-05-02 16:34:41 -0400

    Display/hide goals and change their status if edited by mentor.

--- app/src/main/java/com/example/mentorapp/HomeFragment.java
+++ app/src/main/java/com/example/mentorapp/HomeFragment.java
@@ -50,7 +50,7 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
     SharedPreferences.Editor editor;
     ImageView CHECKMARK_1, CHECKMARK_2, CHECKMARK_3, CHECKMARK_4;
     FloatingActionButton fab;
-    TextView FEEDBACK, GOAL_1, GOAL_2, GOAL_3, GOAL_4, SEMESTER;
+    TextView FEEDBACK, GOAL_1, GOAL_2, GOAL_3, GOAL_4, SEMESTER, GOALS, M_TITLE;
     String url = "https://web.njit.edu/~kas58/mentorDemo/query.php";
     AlertDialog dialog;
     Boolean c1 = false, c2 = false, c3 = false, c4 = false;
@@ -63,7 +63,7 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
         STUDENT = getActivity().getSharedPreferences("STUDENT", Context.MODE_PRIVATE);
         MENTOR = getActivity().getSharedPreferences("MENTOR", Context.MODE_PRIVATE);
         USER_TYPE = getActivity().getSharedPreferences("USER_TYPE", Context.MODE_PRIVATE);
-        fab  = view.findViewById(R.id.m_fab);
+        fab = view.findViewById(R.id.m_fab);
 
         CHECKMARK_1 = (ImageView) view.findViewById(R.id.checkmark1);
         CHECKMARK_2 = (ImageView) view.findViewById(R.id.checkmark2);
@@ -79,9 +79,11 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
         GOAL_2 = view.findViewById(R.id.goal2);
         GOAL_3 = view.findViewById(R.id.goal3);
         GOAL_4 = view.findViewById(R.id.goal4);
+        GOALS = view.findViewById(R.id.goals);
 
         SEMESTER = view.findViewById(R.id.semester);
         FEEDBACK = (TextView) view.findViewById(R.id.feedback);
+        M_TITLE = view.findViewById(R.id.textView7);
 
         dialog = new AlertDialog.Builder(getContext()).create();
         dialog.setTitle("Choose Option");
@@ -89,6 +91,12 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
         dialog.setButton(AlertDialog.BUTTON_POSITIVE, "Goals", new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int which) {
                 dialog.dismiss();
+                Intent intent = new Intent(getContext(), EditGoals.class);
+                intent.putExtra("goal1", GOAL_1.getText().toString());
+                intent.putExtra("goal2", GOAL_2.getText().toString());
+                intent.putExtra("goal3", GOAL_3.getText().toString());
+                intent.putExtra("goal4", GOAL_4.getText().toString());
+                startActivity(intent);
             }
         });
         dialog.setButton(AlertDialog.BUTTON_NEUTRAL, "Message", new DialogInterface.OnClickListener() {
@@ -104,85 +112,75 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
     public void onStart() {
         super.onStart();
         changeSemesterYear(SEMESTER);
-
-        if(isStudent(USER_TYPE)) {
-            /* String Request to Receive Any Feedback from Mentor*/
-            RequestQueue queue = Volley.newRequestQueue(view.getContext());
-            StringRequest stringRequest_1 = new StringRequest(Request.Method.POST, url,
-                    new Response.Listener<String>() {
-                        @Override
-                        public void onResponse(String response) {
-                            if (response.equals("empty")) {
-                                FEEDBACK.setText("No new feedback from " + MENTOR.getString("fname", null));
-                            } else {
-                                FEEDBACK.setText(response);
-                            }
+        /* String Request to Receive Any Feedback from Mentor*/
+        RequestQueue queue = Volley.newRequestQueue(view.getContext());
+        StringRequest stringRequest_1 = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        if (response.equals("empty")) {
+                            FEEDBACK.setText("No new feedback from " + MENTOR.getString("fname", null));
+                        } else {
+                            FEEDBACK.setText(response);
                         }
-                    }, new Response.ErrorListener() {
-                @Override
-                public void onErrorResponse(VolleyError error) {
-                    System.out.println(error);
-                }
-            }) {
-                @Override
-                protected Map<String, String> getParams() {
-                    Map<String, String> params = new HashMap<String, String>();
-                    params.put("action", "getFeedback");
-                    params.put("ucid", STUDENT.getString("ucid", null));
-                    params.put("mentor", MENTOR.getString("ucid", null));  //Change this later <------
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                System.out.println(error);
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("action", "getFeedback");
+                params.put("ucid", STUDENT.getString("ucid", null));
+                params.put("mentor", MENTOR.getString("ucid", null));  //Change this later <------
 
-                    return params;
-                }
-            };
+                return params;
+            }
+        };
 
-            StringRequest stringRequest_2 = new StringRequest(Request.Method.POST, url,
-                    new Response.Listener<String>() {
-                        @Override
-                        public void onResponse(String response) {
+        StringRequest stringRequest_2 = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        System.out.println(response);
+                        if (response.equals("empty")) {
                             System.out.println(response);
-                            if (response.equals("empty")) {
-                                System.out.println(response);
-                                GOAL_1.setText("No new goals from " + MENTOR.getString("fname", null));
-                                GOAL_2.setText("No new goals from " + MENTOR.getString("fname", null));
-                                GOAL_3.setText("No new goals from " + MENTOR.getString("fname", null));
-                                GOAL_4.setText("No new goals from " + MENTOR.getString("fname", null));
-                                System.out.println(response);
-                            } else {
-                                String[] goals = response.split("\\|");
-                                c1 = loadGoal(goals[0], GOAL_1, CHECKMARK_1);
-                                c2 = loadGoal(goals[1], GOAL_2, CHECKMARK_2);
-                                c3 = loadGoal(goals[2], GOAL_3, CHECKMARK_3);
-                                c4 = loadGoal(goals[3], GOAL_4, CHECKMARK_4);
-                                System.out.println(Arrays.toString(goals));
-                            }
+                            GOAL_1.setText("No new goals from " + MENTOR.getString("fname", null));
+                            GOAL_2.setText("No new goals from " + MENTOR.getString("fname", null));
+                            GOAL_3.setText("No new goals from " + MENTOR.getString("fname", null));
+                            GOAL_4.setText("No new goals from " + MENTOR.getString("fname", null));
+                        } else {
+                            String[] goals = response.split("\\|");
+                            c1 = loadGoal(goals[0], GOAL_1, CHECKMARK_1);
+                            c2 = loadGoal(goals[1], GOAL_2, CHECKMARK_2);
+                            c3 = loadGoal(goals[2], GOAL_3, CHECKMARK_3);
+                            c4 = loadGoal(goals[3], GOAL_4, CHECKMARK_4);
+                            System.out.println(Arrays.toString(goals));
                         }
-                    }, new Response.ErrorListener() {
-                @Override
-                public void onErrorResponse(VolleyError error) {
-                    System.out.println(error);
-                }
-            }) {
-                @Override
-                protected Map<String, String> getParams() {
-                    Map<String, String> params = new HashMap<String, String>();
-                    params.put("action", "getGoals");
-                    params.put("ucid", STUDENT.getString("ucid", null));
-                    params.put("mentor", MENTOR.getString("ucid", null));
-                    return params;
-                }
-            };
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                System.out.println(error);
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("action", "getGoals");
+                params.put("ucid", STUDENT.getString("ucid", null));
+                params.put("mentor", MENTOR.getString("ucid", null));
+                return params;
+            }
+        };
 
-            queue.add(stringRequest_1);
-            queue.add(stringRequest_2);
+        queue.add(stringRequest_1);
+        queue.add(stringRequest_2);
 
-            fab.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    dialog.show();
-                }
-            });
-        }
-        else{
+        if (!isStudent(USER_TYPE)) {
             fab.show();
             fab.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -190,13 +188,14 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
                     dialog.show();
                 }
             });
-            //If your a mentor, do this below...
+            GOALS.setText("Your Mentee's Goals");
+            M_TITLE.setText("Your message to Mentee:");
         }
     }
 
     @Override
     public void onClick(View v) {
-        if(isStudent(USER_TYPE)){
+        if (isStudent(USER_TYPE)) {
             switch (v.getId()) {
                 case R.id.checkmark1:
                     if (!c1) {
@@ -266,31 +265,37 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
                 default:
                     break;
             }
-        }
-        else{
+        } else {
 
         }
     }
 
-    private boolean loadGoal(String goal, TextView tv_goal, ImageView ck){
+    private boolean loadGoal(String goal, TextView tv_goal, ImageView ck) {
 
         boolean s = false;
-        String [] data = goal.split("\\\\");
+        String[] data = goal.split("\\\\");
         tv_goal.setText(data[0]);
-        if(data[1].equals("1")){
+        if (data[1].equals("1")) {
             s = true;
+            tv_goal.setVisibility(View.VISIBLE);
+            ck.setVisibility(View.VISIBLE);
             ck.setImageResource(R.drawable.ic_check_green);
-        }
-        else if(data[1].equals("0")){
+        } else if (data[1].equals("0")) {
             s = false;
+            tv_goal.setVisibility(View.VISIBLE);
+            ck.setVisibility(View.VISIBLE);
             ck.setImageResource(R.drawable.ic_check_circle);
+        } else if (data[1].equals("3")) {
+            s = false;
+            tv_goal.setVisibility(View.INVISIBLE);
+            ck.setVisibility(View.INVISIBLE);
         }
 
         return s;
     }
 
     private void changeGoalStatus(View v, String url, final String action,
-                                final String currentUser, final String otherUser, final String goal, final String status){
+                                  final String currentUser, final String otherUser, final String goal, final String status) {
 
         RequestQueue queue = Volley.newRequestQueue(v.getContext());
         StringRequest request = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
@@ -318,16 +323,15 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
         queue.add(request);
     }
 
-    private void postToast(String status){
+    private void postToast(String status) {
 
-        if(status.equals("0")){
+        if (status.equals("0")) {
             Context context = getContext();
             CharSequence text = "Goal Incomplete";
             int duration = Toast.LENGTH_SHORT;
             Toast toast = Toast.makeText(context, text, duration);
             toast.show();
-        }
-        else if(status.equals("1")){
+        } else if (status.equals("1")) {
             Context context = getContext();
             CharSequence text = "Goal Complete";
             int duration = Toast.LENGTH_SHORT;
@@ -337,31 +341,25 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
 
     }
 
-    private void changeSemesterYear(TextView semester){
+    private void changeSemesterYear(TextView semester) {
         Calendar cal = Calendar.getInstance();
         int month = cal.get(Calendar.MONTH);
         String year = Integer.toString(cal.get(Calendar.YEAR));
         System.out.println(year);
 
-        if(month > 7){
+        if (month > 7) {
             semester.setText("Fall '" + year);
-        }
-
-        else if(month < 5){
+        } else if (month < 5) {
             semester.setText("Spring '" + year);
-        }
-
-        else if(4 < month || month < 8){
+        } else if (4 < month || month < 8) {
             semester.setText("Summer '" + year);
         }
     }
 
     private boolean isStudent(SharedPreferences type) {
-        if(type.getString("type", null).equals("student"))
-        {
+        if (type.getString("type", null).equals("student")) {
             return true;
-        }
-        else{
+        } else {
             return false;
         }
     }
