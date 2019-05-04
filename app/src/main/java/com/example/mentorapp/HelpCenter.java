e00eafff2a4fc10bb1266bc2e6df79584d530246
Kevin Solorzano <solorzke@gmail.com>
2019-05-03 23:42:05 -0400

    Add HelpCenter Class/XML layouts to display when clicked in Login

--- app/src/main/java/com/example/mentorapp/HelpCenter.java
+++ app/src/main/java/com/example/mentorapp/HelpCenter.java
@@ -1,13 +1,26 @@
 package com.example.mentorapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class HelpCenter extends AppCompatActivity {
+
+    ImageView back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help_center);
+
+        back = findViewById(R.id.back);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
     }
 }
