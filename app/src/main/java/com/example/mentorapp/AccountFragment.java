c5907247b18a375bde94eacd2a7d9623a630b20c
Kevin Solorzano <solorzke@gmail.com>
2019-05-03 23:50:47 -0400

    Add a ViewPager and TabLayer to Account to swipe between two fragments

--- app/src/main/java/com/example/mentorapp/AccountFragment.java
+++ app/src/main/java/com/example/mentorapp/AccountFragment.java
@@ -1,43 +1,61 @@
 package com.example.mentorapp;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTabHost;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ExpandableListAdapter;
 import android.widget.ExpandableListView;
+import android.widget.TextView;
 
 import com.example.mentorapp.Account.MentorFragment;
 import com.example.mentorapp.Account.StudentFragment;
+import com.example.mentorapp.Events.MeetingDetails;
+import com.example.mentorapp.Events.MeetingStatus;
+import com.example.mentorapp.model.TabAdapter;
+
 import java.util.HashMap;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class AccountFragment extends Fragment {
 
-    private FragmentTabHost tabHost;
-
-    public AccountFragment() {
-    }
+    private TabAdapter adapter;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    private int[] tabIcons = {
+            R.drawable.ic_student,
+            R.drawable.ic_mentor,
+    };
+    TextView cancel;
+    String url = "https://web.njit.edu/~kas58/mentorDemo/query.php";
+    SharedPreferences SESSION;
+    Timer timer;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        tabHost = new FragmentTabHost(getActivity());
-        tabHost.setup(getActivity(), getChildFragmentManager(), R.layout.fragment_account);
+        View view = inflater.inflate(R.layout.fragment_account, container, false);
 
-        Bundle arg1 = new Bundle();
-        arg1.putInt("Arg for Frag1", 1);
-        tabHost.addTab(tabHost.newTabSpec("Tab1").setIndicator("Student"),
-                StudentFragment.class, arg1);
+        viewPager = (ViewPager) view.findViewById(R.id.viewPager);
+        tabLayout = (TabLayout) view.findViewById(R.id.tabLayout);
+        adapter = new TabAdapter(getActivity().getSupportFragmentManager());
+        adapter.addFragment(new StudentFragment(), "Student");
+        adapter.addFragment(new MentorFragment(), "Mentor");
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);
+        tabLayout.getTabAt(0).setIcon(tabIcons[0]);
+        tabLayout.getTabAt(1).setIcon(tabIcons[1]);
 
-        Bundle arg2 = new Bundle();
-        arg2.putInt("Arg for Frag2", 2);
-        tabHost.addTab(tabHost.newTabSpec("Tab2").setIndicator("Mentor"),
-                MentorFragment.class, arg2);
-        return tabHost;
+        return view;
 
     }
+
 }
