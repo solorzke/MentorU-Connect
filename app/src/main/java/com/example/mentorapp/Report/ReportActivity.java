29241358c82f8baccd9f312077091f1df4add156
Kevin Solorzano <solorzke@gmail.com>
2019-06-17 18:16:27 -0400

    Clicking 'Next' takes you to the next activity to verify the claim.

--- app/src/main/java/com/example/mentorapp/Report/ReportActivity.java
+++ app/src/main/java/com/example/mentorapp/Report/ReportActivity.java
@@ -1,5 +1,6 @@
 package com.example.mentorapp.Report;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,10 +9,11 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+
 import com.example.mentorapp.R;
 import com.example.mentorapp.SideBar;
 
-public class ReportActivity extends AppCompatActivity implements View.OnClickListener{
+public class ReportActivity extends AppCompatActivity implements View.OnClickListener {
 
     TextView NEXT, CANCEL, REPORTED_USER, ACTIVITY, HELP_CENTER;
     EditText user_input;
@@ -21,8 +23,7 @@ public class ReportActivity extends AppCompatActivity implements View.OnClickLis
     String intentExtras, name;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report);
         NEXT = findViewById(R.id.next);
@@ -48,8 +49,7 @@ public class ReportActivity extends AppCompatActivity implements View.OnClickLis
     }
 
     @Override
-    protected void onStart()
-    {
+    protected void onStart() {
         super.onStart();
 
         //Set Reported User & activity
@@ -67,17 +67,21 @@ public class ReportActivity extends AppCompatActivity implements View.OnClickLis
     }
 
     @Override
-    public void onClick(View v)
-    {
-        switch (v.getId())
-        {
+    public void onClick(View v) {
+        switch (v.getId()) {
             case R.id.ab_img:
                 startActivity(new Intent(getApplicationContext(), SideBar.class));
                 finish();
                 break;
 
             case R.id.next:
-                //startActivity(new Intent(getApplicationContext(), Activity.class));
+                boolean [] list = {r1, r2, r3, oth};
+                Intent goToNext = new Intent(getApplicationContext(), VerifyReportActivity.class);
+                goToNext.putExtra("name", REPORTED_USER.getText().toString());
+                goToNext.putExtra("msg", ACTIVITY.getText().toString());
+                goToNext.putExtra("reasons", list);
+                goToNext.putExtra("other_detail", user_input.getText().toString());
+                startActivity(goToNext);
                 finish();
                 break;
 
@@ -87,14 +91,12 @@ public class ReportActivity extends AppCompatActivity implements View.OnClickLis
 
             case R.id.reason_1:
                 /* If radio button is pressed already */
-                if(r1)
-                {
+                if (r1) {
                     this.r1 = false;
                     this.th_1.setImageResource(android.R.drawable.radiobutton_off_background);
                 }
                 /* If radio button isn't pressed already */
-                else
-                {
+                else {
                     this.r1 = true;
                     this.th_1.setImageResource(android.R.drawable.radiobutton_on_background);
                 }
@@ -102,14 +104,12 @@ public class ReportActivity extends AppCompatActivity implements View.OnClickLis
 
             case R.id.reason_2:
                 /* If radio button is pressed already */
-                if(r2)
-                {
+                if (r2) {
                     this.r2 = false;
                     this.th_2.setImageResource(android.R.drawable.radiobutton_off_background);
                 }
                 /* If radio button isn't pressed already */
-                else
-                {
+                else {
                     this.r2 = true;
                     this.th_2.setImageResource(android.R.drawable.radiobutton_on_background);
                 }
@@ -117,14 +117,12 @@ public class ReportActivity extends AppCompatActivity implements View.OnClickLis
 
             case R.id.reason_3:
                 /* If radio button is pressed already */
-                if(r3)
-                {
+                if (r3) {
                     this.r3 = false;
                     this.th_3.setImageResource(android.R.drawable.radiobutton_off_background);
                 }
                 /* If radio button isn't pressed already */
-                else
-                {
+                else {
                     this.r3 = true;
                     this.th_3.setImageResource(android.R.drawable.radiobutton_on_background);
                 }
@@ -132,15 +130,13 @@ public class ReportActivity extends AppCompatActivity implements View.OnClickLis
 
             case R.id.other:
                 /* If radio button is pressed already */
-                if(oth)
-                {
+                if (oth) {
                     this.oth = false;
                     this.th_other.setImageResource(android.R.drawable.radiobutton_off_background);
                     user_input.setVisibility(View.GONE);
                 }
                 /* If radio button isn't pressed already */
-                else
-                {
+                else {
                     this.oth = true;
                     this.th_other.setImageResource(android.R.drawable.radiobutton_on_background);
                     user_input.setVisibility(View.VISIBLE);
