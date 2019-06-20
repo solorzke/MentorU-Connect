ebc1cb7dee6aacf630f6ecd01b9cf2b0ee9ee2bd
Kevin Solorzano <solorzke@gmail.com>
2019-06-20 00:15:39 -0400

    Reformat date string into the 'Y-M-D' format to submit to the DB server.

--- app/src/main/java/com/example/mentorapp/model/DateTimeFormat.java
+++ app/src/main/java/com/example/mentorapp/model/DateTimeFormat.java
@@ -2,11 +2,19 @@ package com.example.mentorapp.model;
 
 public class DateTimeFormat {
 
-    public static String formatDate(String date){
-
-        //Y-M-D -> M/D/YYYY Ex: 2019-05-27 -> 05/27/2019
+    public static String formatDate(String date)
+    {
+        /* Y-M-D -> M/D/YYYY Ex: 2019-05-27 -> 05/27/2019 */
         String [] d = date.split("-"); //Should be 3 indices
         String new_date = d[1] + "/" + d[2] + "/" + d[0];
+        return new_date;
+    }
+
+    public static String formatDateSQL(String date)
+    {
+        /* MM/DD/YYYY -> Y-M-D Ex: 09/15/1995 -> 1995-09-15 */
+        String [] d = date.split("/");
+        String new_date = d[2] + "-" + d[0] + "-" + d[1];
         return new_date;
     }
 }
