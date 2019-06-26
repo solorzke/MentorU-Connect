e0b31fe5f5bbc33f29dcf49a97128e4eb28a3152
Kevin Solorzano <solorzke@gmail.com>
2019-06-26 19:38:07 -0400

    Add cancel/submit buttons, implement Click Listener interface.

--- app/src/main/java/com/example/mentorapp/CoachingLog/RequestMeeting.java
+++ app/src/main/java/com/example/mentorapp/CoachingLog/RequestMeeting.java
@@ -10,15 +10,12 @@ import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
-
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -26,26 +23,28 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.example.mentorapp.R;
-
+import com.example.mentorapp.model.DateTimeFormat;
+import com.example.mentorapp.model.Validate;
 import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Map;
 
-public class RequestMeeting extends AppCompatActivity {
-
+public class RequestMeeting extends AppCompatActivity implements View.OnClickListener
+{
     EditText event_location, event_title, event_purpose, event_start_time, event_end_time, event_date;
-    private Button submit;
-    private SharedPreferences SESSION, OTHER_USER;
-    private DatePickerDialog dialog;
-    private DatePickerDialog.OnDateSetListener mDateSetListener;
+    private TextView submit, cancel;
+    private SharedPreferences sender, receiver, user_type;
+    private DatePickerDialog date;
     private TimePickerDialog timePickerDialog;
+    private Calendar calendar;
+    int hour, min;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_request_meeting);
-        SESSION = getSharedPreferences("USER", Context.MODE_PRIVATE);
-        OTHER_USER = getSharedPreferences("MENTOR", Context.MODE_PRIVATE);
+        user_type = getSharedPreferences("USER_TYPE", Context.MODE_PRIVATE);
         event_location = findViewById(R.id.event_location);
         event_title = findViewById(R.id.event_title);
         event_purpose = findViewById(R.id.event_purpose);
@@ -53,114 +52,118 @@ public class RequestMeeting extends AppCompatActivity {
         event_start_time = findViewById(R.id.event_start_time);
         event_end_time = findViewById(R.id.event_end_time);
         submit = findViewById(R.id.create_event_submit);
+        cancel = findViewById(R.id.cancel);
 
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayOptions(android.support.v7.app.ActionBar.DISPLAY_SHOW_CUSTOM);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
-        event_date.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Calendar cal = Calendar.getInstance();
-                int year = cal.get(Calendar.YEAR);
-                int month = cal.get(Calendar.MONTH);
-                int day = cal.get(Calendar.DAY_OF_MONTH);
-
-                dialog = new DatePickerDialog(RequestMeeting.this,
-                        android.R.style.Theme_Holo_Light_Dialog_MinWidth, mDateSetListener,
-                        year, month, day);
-                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-                dialog.show();
-            }
-        });
-
-        mDateSetListener = new DatePickerDialog.OnDateSetListener() {
-            @Override
-            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-                month = month + 1;
-                event_date.setText(Integer.toString(month) + '/' + Integer.toString(dayOfMonth) + '/' +
-                        Integer.toString(year));
-            }
-        };
-
-        event_start_time.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Calendar cal = Calendar.getInstance();
-                int hour = cal.get(Calendar.HOUR_OF_DAY);
-                int min = cal.get(Calendar.MINUTE);
-                timePickerDialog = new TimePickerDialog(RequestMeeting.this,
-                        new TimePickerDialog.OnTimeSetListener() {
-                            @Override
-                            public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
-                                String [] data = format12HourTime(selectedHour, selectedMinute);
-                                event_start_time.setText(data[0] + ":" + data[1] + " " + data[2]);
-                            }
-                        }, hour, min, false);
-                timePickerDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-                timePickerDialog.show();
-            }
-        });
-
-        event_end_time.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Calendar cal = Calendar.getInstance();
-                int hour = cal.get(Calendar.HOUR_OF_DAY);
-                int min = cal.get(Calendar.MINUTE);
-                timePickerDialog = new TimePickerDialog(RequestMeeting.this,
-                        new TimePickerDialog.OnTimeSetListener() {
-                            @Override
-                            public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
-                                String [] data = format12HourTime(selectedHour, selectedMinute);
-                                event_end_time.setText(data[0] + ":" + data[1] + " " + data[2]);
-                            }
-                        }, hour, min, false);
-                timePickerDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-                timePickerDialog.show();
-            }
-        });
-
-        submit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                EditText [] event = {event_title, event_location, event_date, event_start_time,
-                        event_end_time, event_purpose};
-                String url = "https://web.njit.edu/~kas58/mentorDemo/Model/index.php";
-                String student = SESSION.getString("ucid", null);
-                String mentor = OTHER_USER.getString("ucid", null);
-                if(checkForm(event)){
-                    sendMeetingRequest(v, url, "requestMeeting", event, student, mentor);
-                    Context context = getApplicationContext();
-                    CharSequence text = "Request Sent!";
-                    int duration = Toast.LENGTH_SHORT;
-                    Toast toast = Toast.makeText(context, text, duration);
-                    toast.show();
-                    onBackPressed();
-                }
-                else{ sendAlert(); }
-            }
-        });
-
-
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                onBackPressed();
-                finish();
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
+        if(Validate.isStudent(user_type))
+        {
+            sender = getSharedPreferences("STUDENT", Context.MODE_PRIVATE);
+            receiver = getSharedPreferences("MENTOR", Context.MODE_PRIVATE);
+        }
+        else
+        {
+            sender = getSharedPreferences("MENTOR", Context.MODE_PRIVATE);
+            receiver = getSharedPreferences("STUDENT", Context.MODE_PRIVATE);
         }
     }
 
-    private void sendMeetingRequest(View v, String url, final String action, final EditText [] event,
-                                  final String currentUser, final String otherUser){
-        RequestQueue queue = Volley.newRequestQueue(v.getContext());
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+        event_date.setOnClickListener(this);
+        event_start_time.setOnClickListener(this);
+        event_end_time.setOnClickListener(this);
+        submit.setOnClickListener(this);
+        cancel.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v)
+    {
+        switch (v.getId())
+        {
+            case R.id.event_date:
+                calendar = Calendar.getInstance();
+                int day = calendar.get(Calendar.DAY_OF_MONTH);
+                int month = calendar.get(Calendar.MONTH);
+                int year = calendar.get(Calendar.YEAR);
+
+                date = new DatePickerDialog(RequestMeeting.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        event_date.setText((month + 1)+"/"+dayOfMonth+"/"+year);
+                    }
+                }, year, month, day);
+                date.show();
+                break;
+
+            case R.id.event_start_time:
+                calendar = Calendar.getInstance();
+                hour = calendar.get(Calendar.HOUR_OF_DAY);
+                min = calendar.get(Calendar.MINUTE);
+                timePickerDialog = new TimePickerDialog(RequestMeeting.this,
+                        new TimePickerDialog.OnTimeSetListener() {
+                            @Override
+                            public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
+                                String [] data = Validate.format12HourTime(selectedHour, selectedMinute);
+                                event_start_time.setText(data[0] + ":" + data[1] + " " + data[2]);
+                            }
+                        }, hour, min, false);
+                timePickerDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.GRAY));
+                timePickerDialog.show();
+                break;
+
+            case R.id.event_end_time:
+                Calendar cal = Calendar.getInstance();
+                hour = cal.get(Calendar.HOUR_OF_DAY);
+                min = cal.get(Calendar.MINUTE);
+                timePickerDialog = new TimePickerDialog(RequestMeeting.this,
+                        new TimePickerDialog.OnTimeSetListener() {
+                            @Override
+                            public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
+                                String [] data = Validate.format12HourTime(selectedHour, selectedMinute);
+                                event_end_time.setText(data[0] + ":" + data[1] + " " + data[2]);
+                            }
+                        }, hour, min, false);
+                timePickerDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.GRAY));
+                timePickerDialog.show();
+                break;
+
+            case R.id.create_event_submit:
+                EditText [] event = {event_title, event_location, event_date, event_start_time,
+                        event_end_time, event_purpose};
+                String url = "https://web.njit.edu/~kas58/mentorDemo/Model/index.php";
+                String student = sender.getString("ucid", null);
+                String mentor = receiver.getString("ucid", null);
+
+                if(Validate.checkForm(event))
+                {
+                    sendMeetingRequest(url, "requestMeeting", event, student, mentor);
+                    CharSequence text = "Request Sent!";
+                    int duration = Toast.LENGTH_SHORT;
+                    Toast toast = Toast.makeText(RequestMeeting.this, text, duration);
+                    toast.show();
+                    onBackPressed();
+                    finish();
+                }
+                else{ sendAlert(); }
+                break;
+
+            case R.id.cancel:
+                onBackPressed();
+                finish();
+                break;
+
+            default:
+                break;
+        }
+
+    }
+
+    private void sendMeetingRequest(String url, final String action, final EditText [] event,
+                                  final String currentUser, final String otherUser)
+    {
+        RequestQueue queue = Volley.newRequestQueue(this);
         StringRequest request = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
@@ -177,7 +180,7 @@ public class RequestMeeting extends AppCompatActivity {
                 Map<String, String> params = new HashMap<String, String>();
                 params.put("title", event[0].getText().toString());
                 params.put("location", event[1].getText().toString());
-                params.put("date", event[2].getText().toString());
+                params.put("date", DateTimeFormat.formatDateSQL(event[2].getText().toString()));
                 params.put("s_time", event[3].getText().toString());
                 params.put("e_time", event[4].getText().toString());
                 params.put("purpose", event[5].getText().toString());
@@ -190,41 +193,8 @@ public class RequestMeeting extends AppCompatActivity {
         queue.add(request);
     }
 
-    private String [] format12HourTime(int hour, int min){
-        int h = hour;
-        String m;
-        String timeset;
-
-        if(hour > 12){  h -= 12; timeset = "PM";  }
-        else if(hour == 0){  h += 12; timeset = "AM";  }
-        else if(hour == 12){  timeset = "PM";  }
-        else{  timeset = "AM";  }
-
-        if(min < 10){ m = "0" + min; }
-        else{  m = String.valueOf(min);  }
-
-        String [] data = {Integer.toString(h), m, timeset};
-        return data;
-    }
-
-    private boolean checkForm(EditText[] event){
-
-        String title = event[0].getText().toString();
-        String loc = event[1].getText().toString();
-        String dt = event[2].getText().toString();
-        String st = event[3].getText().toString();
-        String et = event[4].getText().toString();
-        String purpose = event[5].getText().toString();
-
-        if(title.equals("") || loc.equals("") || dt.equals("") || st.equals("") || et.equals("")
-                || purpose.equals("")){
-            return false;
-        }
-        return true;
-    }
-
-    private void sendAlert(){
-
+    private void sendAlert()
+    {
         AlertDialog alert = new AlertDialog.Builder(RequestMeeting.this).create();
         alert.setTitle("Incomplete Form");
         alert.setMessage("Please complete all the fields.");
