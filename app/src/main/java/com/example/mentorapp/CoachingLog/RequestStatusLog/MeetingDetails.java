58c73be2fb96160c9de01ba8346ced2afa593579
Kevin Solorzano <solorzke@gmail.com>
2019-07-02 15:53:34 -0400

    Send an Intent URI to Calendar app to create event with the meeting info.

--- app/src/main/java/com/example/mentorapp/CoachingLog/RequestStatusLog/MeetingDetails.java
+++ app/src/main/java/com/example/mentorapp/CoachingLog/RequestStatusLog/MeetingDetails.java
@@ -3,7 +3,9 @@ package com.example.mentorapp.CoachingLog.RequestStatusLog;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.CalendarContract;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -23,6 +25,7 @@ import com.android.volley.toolbox.Volley;
 import com.example.mentorapp.R;
 import com.example.mentorapp.model.DateTimeFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -35,6 +38,7 @@ public class MeetingDetails extends Fragment
     Button respond;
     ArrayList<String> meeting_request = new ArrayList<>();
     Toast toast;
+    boolean done = false;
 
     /* When the fragment view is created, initialize necessary objects */
     @Nullable
@@ -92,11 +96,6 @@ public class MeetingDetails extends Fragment
             @Override
             public void onResponse(String response) {
                 System.out.println("Server response: "+response);
-                Context context = getContext();
-                CharSequence text = "Meeting Confirmed!";
-                int duration = Toast.LENGTH_SHORT;
-                Toast toast = Toast.makeText(context, text, duration);
-                toast.show();
             }
         }, new Response.ErrorListener() {
             @Override
@@ -130,7 +129,7 @@ public class MeetingDetails extends Fragment
                 toast = Toast.makeText(getContext(), "Meeting Accepted", Toast.LENGTH_SHORT);
                 toast.show();
                 dialog.dismiss();
-                getActivity().onBackPressed();
+                createEvent(meeting_request);
             }
         });
 
@@ -149,6 +148,55 @@ public class MeetingDetails extends Fragment
 
         alert.show();
     }
+
+    /* Function to create the Calendar event using its API. */
+    private void createEvent(ArrayList <String> details)
+    {
+        /* array = ['id', 'sender', 'receiver', 'title', 'e_date', 'start_time', 'end_time',
+                    'location', 'purpose', 'status'] */
+        String formatted_date = DateTimeFormat.formatDate(details.get(4));
+        int [] s_formatTime = DateTimeFormat.format24HourTime(details.get(5));
+        int [] e_formatTime = DateTimeFormat.format24HourTime(details.get(6));
+        int [] s_timestamp = DateTimeFormat.parseDateAndTime(formatted_date, s_formatTime[0], s_formatTime[1]);
+        int [] e_timestamp = DateTimeFormat.parseDateAndTime(formatted_date, e_formatTime[0], e_formatTime[1]);
+
+        /* Convert date/time to milliseconds */
+        Calendar beginTime = Calendar.getInstance();
+        beginTime.set(s_timestamp[0], s_timestamp[1] -= 1, s_timestamp[2], s_timestamp[3], s_timestamp[4]);
+        long startMillis = beginTime.getTimeInMillis();
+
+        Calendar endTime = Calendar.getInstance();
+        endTime.set(e_timestamp[0], e_timestamp[1] -= 1, e_timestamp[2], e_timestamp[3], e_timestamp[4]);
+        long endMillis = endTime.getTimeInMillis();
+
+        /* Pass a URI Intent to the Calendar API along with the form data, to take you to its interface */
+        Intent intent = new Intent(Intent.ACTION_INSERT)
+                .setData(CalendarContract.Events.CONTENT_URI)
+                .putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, startMillis)
+                .putExtra(CalendarContract.EXTRA_EVENT_END_TIME, endMillis)
+                .putExtra(CalendarContract.Events.TITLE, details.get(3))
+                .putExtra(CalendarContract.Events.DESCRIPTION, details.get(8))
+                .putExtra(CalendarContract.Events.EVENT_LOCATION, details.get(7))
+                .putExtra(CalendarContract.Events.AVAILABILITY, CalendarContract.Events.AVAILABILITY_BUSY)
+                .putExtra(Intent.EXTRA_EMAIL, getEmail());
+        this.done = true;
+        startActivity(intent);
+        getActivity().onBackPressed();
+    }
+
+    private String getEmail()
+    {
+        if(meeting_with.getText().toString().equals(getActivity().getSharedPreferences("MENTOR",
+                Context.MODE_PRIVATE).getString("ucid", null)))
+        {
+            return getActivity().getSharedPreferences("MENTOR",
+                    Context.MODE_PRIVATE).getString("email", null);
+        }
+        else
+            return getActivity().getSharedPreferences("STUDENT",
+                    Context.MODE_PRIVATE).getString("email", null);
+    }
+
 
 
 }
