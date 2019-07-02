75c0f22442aec30eabebaec0751913efb6f0c22e
Kevin Solorzano <solorzke@gmail.com>
2019-07-01 20:10:23 -0400

    Confirm the meeting response to the DB, and format the dates/times.

--- app/src/main/java/com/example/mentorapp/CoachingLog/RequestStatusLog/MeetingDetails.java
+++ app/src/main/java/com/example/mentorapp/CoachingLog/RequestStatusLog/MeetingDetails.java
@@ -21,18 +21,20 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.example.mentorapp.R;
+import com.example.mentorapp.model.DateTimeFormat;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 public class MeetingDetails extends Fragment
 {
-    String url = "https://web.njit.edu/~kas58/mentorDemo/Model/index.php";
+    String url = "https://web.njit.edu/~kas58/mentorDemo/Model/index.php", type;
     EditText title, location, purpose, date, s_time, e_time;
     TextView meeting_with;
     View view;
     Button respond;
     ArrayList<String> meeting_request = new ArrayList<>();
+    Toast toast;
 
     /* When the fragment view is created, initialize necessary objects */
     @Nullable
@@ -49,38 +51,47 @@ public class MeetingDetails extends Fragment
         meeting_with = view.findViewById(R.id.meeting_user);
         respond = view.findViewById(R.id.respond);
         meeting_request = getActivity().getIntent().getStringArrayListExtra("meeting_details");
+        type = getActivity().getIntent().getExtras().getString("type");
         return view;
     }
 
     @Override
-    public void onStart() {
+    public void onStart()
+    {
         super.onStart();
         /* array = ['id', 'sender', 'receiver', 'title', 'e_date', 'start_time', 'end_time',
                     'location', 'purpose', 'status'] */
         title.setText(meeting_request.get(3));
         location.setText(meeting_request.get(7));
         purpose.setText(meeting_request.get(8));
-        date.setText(meeting_request.get(4));
-        s_time.setText(meeting_request.get(5));
-        e_time.setText(meeting_request.get(6));
+        date.setText(DateTimeFormat.formatDate(meeting_request.get(4)));
+        s_time.setText(DateTimeFormat.format12HourTimeAsString(meeting_request.get(5)));
+        e_time.setText(DateTimeFormat.format12HourTimeAsString(meeting_request.get(6)));
         meeting_with.setText(meeting_request.get(2));
 
-        respond.setOnClickListener(new View.OnClickListener()
+        if(meeting_request.get(9).equals("3") && type.equals("receiver"))
         {
-            @Override
-            public void onClick(View v) {
-                sendAlert();
-            }
-        });
+            respond.setOnClickListener(new View.OnClickListener()
+            {
+                @Override
+                public void onClick(View v) {
+                    sendAlert();
+                }
+            });
+        }
+        else
+            respond.setVisibility(View.GONE);
     }
 
     /* Sends a web server request to change the meeting request status to approved */
 
-    private void confirmMeeting(View v, String url, final String action, final String currentUser) {
+    private void confirmMeeting(View v, String url, final String row_id, final String status)
+    {
         RequestQueue queue = Volley.newRequestQueue(v.getContext());
         StringRequest request = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
+                System.out.println("Server response: "+response);
                 Context context = getContext();
                 CharSequence text = "Meeting Confirmed!";
                 int duration = Toast.LENGTH_SHORT;
@@ -90,14 +101,16 @@ public class MeetingDetails extends Fragment
         }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-                System.out.println(error);
+                System.out.println("Volley Error: " + error);
+                error.printStackTrace();
             }
         }) {
             @Override
             protected Map<String, String> getParams() {
                 Map<String, String> params = new HashMap<String, String>();
-                params.put("action", action);
-                params.put("currentUser", currentUser);
+                params.put("action", "confirm");
+                params.put("row_id", row_id);
+                params.put("status", status);
                 return params;
             }
         };
@@ -112,7 +125,12 @@ public class MeetingDetails extends Fragment
         alert.setButton(android.support.v7.app.AlertDialog.BUTTON_POSITIVE, "Accept",
                 new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int which) {
+                confirmMeeting(view, url, meeting_request.get(0), "1");
+                respond.setVisibility(View.GONE);
+                toast = Toast.makeText(getContext(), "Meeting Accepted", Toast.LENGTH_SHORT);
+                toast.show();
                 dialog.dismiss();
+                getActivity().onBackPressed();
             }
         });
 
@@ -120,7 +138,12 @@ public class MeetingDetails extends Fragment
                 new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
+                confirmMeeting(view, url, meeting_request.get(0), "2");
+                respond.setVisibility(View.GONE);
+                toast = Toast.makeText(getContext(), "Meeting Declined", Toast.LENGTH_SHORT);
+                toast.show();
                 dialog.dismiss();
+                getActivity().onBackPressed();
             }
         });
 
