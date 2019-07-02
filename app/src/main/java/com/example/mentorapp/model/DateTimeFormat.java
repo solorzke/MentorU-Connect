b99dc8b4b4aec5c7ce8120238647e2f95592ea15
Kevin Solorzano <solorzke@gmail.com>
2019-07-02 15:52:42 -0400

    Function for formatting standard 12 hour time into 24 hour time

--- app/src/main/java/com/example/mentorapp/model/DateTimeFormat.java
+++ app/src/main/java/com/example/mentorapp/model/DateTimeFormat.java
@@ -5,6 +5,7 @@ import java.util.GregorianCalendar;
 
 public class DateTimeFormat {
 
+    /* Format the date from the DB to MM/DD/YYYY */
     public static String formatDate(String date)
     {
         /* Y-M-D -> M/D/YYYY Ex: 2019-05-27 -> 05/27/2019 */
@@ -13,6 +14,7 @@ public class DateTimeFormat {
         return new_date;
     }
 
+    /* Format the date to the proper form for SQL query */
     public static String formatDateSQL(String date)
     {
         /* MM/DD/YYYY -> Y-M-D Ex: 09/15/1995 -> 1995-09-15 */
@@ -21,6 +23,7 @@ public class DateTimeFormat {
         return new_date;
     }
 
+    /* Set the month to find the weeks in between */
     public static int getFullWeeks(int month)
     {
         Calendar start = Calendar.getInstance();
@@ -33,6 +36,7 @@ public class DateTimeFormat {
         return getNrWeeksBetween(start, end);
     }
 
+    /* Return the weeks in between start date and end date */
     private static int getNrWeeksBetween(Calendar start, Calendar end)
     {
         int weeks = 0;
@@ -50,6 +54,7 @@ public class DateTimeFormat {
         return weeks;
     }
 
+    /* Format the 24 hour time to 12 Hour time */
     public static String [] format12HourTime(int hour, int min)
     {
         int h = hour;
@@ -68,6 +73,7 @@ public class DateTimeFormat {
         return data;
     }
 
+    /* Format 24 Hour Time to 12 Hour time and return as a string */
     public static String format12HourTimeAsString(String str)
     {
         String [] date = str.split(":");
@@ -92,9 +98,26 @@ public class DateTimeFormat {
     }
 
     /* Parse dates, hours, mins to convert into milliseconds later */
-    public static int[] parseDateAndTime(String date, int hour, int min) {
+    public static int[] parseDateAndTime(String date, int hour, int min)
+    {
         String[] d = date.split("/");
         int[] dt = {Integer.parseInt(d[2]), Integer.parseInt(d[0]), Integer.parseInt(d[1]), hour, min};
         return dt;
+    }
+
+    /* Format 12 Hour time to 24 Hour time */
+    public static int[] format24HourTime(String str)
+    {
+        String [] date = str.split(":");
+        int hour = Integer.parseInt(date[0]);
+        int min = Integer.parseInt(date[1]);
+        int h = hour;
+
+        if(date[2].equals("PM") && hour > 12)
+            h += 12;
+        else if(date[2].equals("PM") && hour == 12)
+            h = 0;
+
+        return new int[]{h, min};
     }
 }
