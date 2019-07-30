8e2c43de547739216dd63a28b4c528e3b66850e9
Kevin Solorzano <solorzke@gmail.com>
2019-07-29 23:36:16 -0400

    UserType class defined to identify current and opposite users.

---
+++ app/src/main/java/com/njit/mentorapp/model/users/UserType.java
@@ -0,0 +1,55 @@
+package com.njit.mentorapp.model.users;
+
+import android.content.Context;
+
+public class UserType
+{
+    private String currentType;
+    private String pairedType;
+    private Context context;
+
+    public UserType(Context context)
+    {
+        this.context = context;
+        String type = context
+                .getSharedPreferences("USER_TYPE", Context.MODE_PRIVATE)
+                .getString("type", null);
+
+        if(type.equals("student"))
+        {
+            this.currentType = "mentee";
+            this.pairedType = "mentor";
+        }
+        else
+        {
+            this.currentType = "mentor";
+            this.pairedType = "mentee";
+        }
+    }
+
+    public String getCurrentUser()
+    {
+        if(currentType.equals("mentee"))
+            return new Mentee(context).getUcid();
+        else
+            return new Mentor(context).getUcid();
+    }
+
+    public String getPairedUser()
+    {
+        if(currentType.equals("mentee"))
+            return new Mentor(context).getUcid();
+        else
+            return new Mentee(context).getUcid();
+    }
+
+    public String getCurrentType()
+    {
+        return this.currentType;
+    }
+
+    public String getPairedType()
+    {
+        return this.pairedType;
+    }
+}
