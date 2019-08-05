579ae2921c56928ef6c2b7572570da8f33df9617
Kevin Solorzano <solorzke@gmail.com>
2019-08-05 18:06:37 -0400

    Add a new callback in the 'FireBaseCallback' interface

--- app/src/main/java/com/njit/mentorapp/model/service/FireBaseCallback.java
+++ app/src/main/java/com/njit/mentorapp/model/service/FireBaseCallback.java
@@ -1,6 +1,10 @@
 package com.njit.mentorapp.model.service;
 
+import com.google.firebase.storage.StorageReference;
+
 public interface FireBaseCallback
 {
     void onCallback(String value);
+
+    void onCallback(StorageReference storageReference);
 }
