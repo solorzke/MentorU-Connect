0a5516c35880b1c76b23fadb7c72dc7b28bf8a3a
Kevin Solorzano <solorzke@gmail.com>
2019-08-08 17:58:12 -0400

    Automatically fill in the recipient's email address

--- app/src/main/java/com/njit/mentorapp/toolbar/SendEmail.java
+++ app/src/main/java/com/njit/mentorapp/toolbar/SendEmail.java
@@ -11,20 +11,20 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
-
 import com.njit.mentorapp.R;
 import com.njit.mentorapp.model.users.User;
 
-public class SendEmail extends AppCompatActivity {
-
+public class SendEmail extends AppCompatActivity
+{
     EditText TO, SUBJECT, BODY;
     ImageView send;
     SharedPreferences USER_TYPE;
-    private User user;
+    private String recipient_email;
     boolean done = false;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_send_email);
         TO = findViewById(R.id.to_email);
@@ -33,15 +33,15 @@ public class SendEmail extends AppCompatActivity {
         send = findViewById(R.id.sendEmail);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayOptions(android.support.v7.app.ActionBar.DISPLAY_SHOW_CUSTOM);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        toolbar.setTitle("Send Email");
+        if(getSupportActionBar() != null){
+            getSupportActionBar().setDisplayOptions(android.support.v7.app.ActionBar.DISPLAY_SHOW_CUSTOM);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            toolbar.setTitle("Send Email");
+        }
         USER_TYPE = getSharedPreferences("USER_TYPE", Context.MODE_PRIVATE);
-
-        if(isStudent(USER_TYPE))
-            user = new User(getApplicationContext(), "Mentee");
-        else
-            user = new User(getApplicationContext(), "Mentor");
+        recipient_email = isStudent(USER_TYPE)
+                ? new User(getApplicationContext(), "Mentor").getEmail()
+                : new User(getApplicationContext(), "Mentee").getEmail();
     }
 
     /* After onCreateView(), collect the form data and send an intent to their preferred email
@@ -49,6 +49,7 @@ public class SendEmail extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
+        TO.setText(recipient_email.equals("N/A") ? "" : recipient_email);
         send.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -64,45 +65,44 @@ public class SendEmail extends AppCompatActivity {
     /* After completing the email process, resume here and send you back to your previous
     * opened page */
     @Override
-    protected void onResume() {
+    protected void onResume()
+    {
         super.onResume();
-        if(done){
+        if(done)
             onBackPressed();
-        }
     }
 
     /* Store email message info and pass over to EMAIL Application of their choice . */
-    public void composeEmail(String address, String subject, String body) {
+    public void composeEmail(String address, String subject, String body)
+    {
         Intent intent = new Intent(Intent.ACTION_SENDTO);
         intent.setData(Uri.parse("mailto:" + address)); // only email apps should handle this
         intent.putExtra(Intent.EXTRA_SUBJECT, subject);
         intent.putExtra(Intent.EXTRA_TEXT, body);
-        if (intent.resolveActivity(getPackageManager()) != null) {
+        if (intent.resolveActivity(getPackageManager()) != null)
             startActivity(intent);
-        }
     }
 
     /* When clicking the back button, go back to the last page. */
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch (item.getItemId())
+        {
             case android.R.id.home:
                 onBackPressed();
                 finish();
                 return true;
+            case 999999999:
+                return false;
             default:
                 return super.onOptionsItemSelected(item);
         }
     }
 
     /* Define who the user type is to pass their email address in the Email API */
-    private boolean isStudent(SharedPreferences type) {
-        if(type.getString("type", null).equals("student"))
-        {
-            return true;
-        }
-        else{
-            return false;
-        }
+    private boolean isStudent(SharedPreferences type)
+    {
+        return type.getString("type", null).equals("student");
     }
 }
